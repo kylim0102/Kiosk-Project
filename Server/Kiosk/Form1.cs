977af975c02a36ee38c1ff2c1c8139ff01885933
hoon <ldh970527@naver.com>
2024-07-09 21:28:21 +0900

    select button page fixed hoon

--- Server/Kiosk/Form1.cs
+++ Server/Kiosk/Form1.cs
@@ -12,8 +12,8 @@ namespace Kiosk
 {
     public partial class Form1 : Form
     {
-        pPanel.Chart chart = new pPanel.Chart();
-        Order.Order_Manage order = new Order.Order_Manage();
+        //pPanel.Chart chart = new pPanel.Chart();
+        //Order.Order_Manage order = new Order.Order_Manage();
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +21,38 @@ namespace Kiosk
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //chart.Show();
+            pPanel.Chart chart = new pPanel.Chart();
             chart.Show();
+
+            /*
+                chart.Show();
+                →  코드 실행 후 차트 패널을 열었다가 닫고 다시 열게 되면
+                    'System.ObjectDisposedException: '삭제된 개체에 액세스할 수 없습니다.' 오류 발생함
+
+                상단에서 해주는 초기화가 버튼 클릭 시마다 필요함.
+
+                ex1)
+                pPanel.Chart chart = new nPanel.Chart();
+                chart.Show();
+
+                ex2)
+                using(pPanel.Chart chart = new nPanel.Chart())
+                {
+                    chart.ShowDialog();
+                }
+
+                ※ ShowDialog()와 Show()의 차이점
+                
+                    1) ShowDialog()의 경우 모달 대화상자로 열림
+                        모달 : 모달 폼이 열려있는 동안에는 사용자가 다른 폼을 사용할 수 없음.
+                               모달이 닫힐 때까지 제어가 해당 폼에 머물게 됨
+                        DialogResult 반환 : 폼이 닫힐 때 DialogResult를 반환하여, 사용자가 폼을 어떻게 닫았는지(Ok, Cancle 등)을 알 수 있음.
+
+                    2) Show()의 경우 비 모달 대화상자로 열림
+                        ShowDialog()와 반대 개념
+                               
+             */
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,7 +62,11 @@ namespace Kiosk
 
         private void button1_Click(object sender, EventArgs e)
         {
-            order.Show();
+            using(Order.Order_Manage order = new Order.Order_Manage())
+            {
+                order.ShowDialog();
+            }
+            
         }
     }
 }
