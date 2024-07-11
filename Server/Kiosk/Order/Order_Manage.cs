fa99af22c0efeef6f33690cc7c3c5d2931df18c2
hoon <ldh970526@naver.com>
2024-07-11 23:19:12 +0900

    Order Screen Connection DataBase And Show

--- Server/Kiosk/Order/Order_Manage.cs
+++ Server/Kiosk/Order/Order_Manage.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Kiosk.pPanel.common;
+using MySql.Data.MySqlClient;
 
 namespace Kiosk.Order
 {
@@ -48,7 +50,68 @@ namespace Kiosk.Order
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
+            MySqlConnection con = oGlobal.GetConnection();
 
+            con.Open();
+
+            string sql = "select * from itemtable";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                int idx = 0, price = 0;
+
+                string item_name = null, content = null, category = null, power = null;
+
+                DateTime regdate;
+
+                TabControl tabControl = menulist;
+
+                TabPage mysql_tab = tabControl.TabPages[3];
+
+                int buttonTop = 10;
+                int buttonSpacing = 30;
+
+                while (reader.Read())
+                {
+                    Console.WriteLine("Reading data for idx: " + reader.GetInt32("idx"));
+
+                    idx = reader.GetInt32("idx");
+                    item_name = reader.GetString("itemName");
+                    price = reader.GetInt32("price");
+                    content = reader.GetString("content");
+                    regdate = reader.GetDateTime("regdate");
+                    category = reader.GetString("category");
+                    power = reader.GetString("on/off");
+                    
+
+
+                    Button button = new Button();
+                    button.Name = "mysql_"+idx;
+                    button.Text = item_name;
+                    button.Tag = new {idx, item_name, price, content, regdate, category, power };
+
+                    // 버튼 위치 설정
+                    button.Top = buttonTop;
+                    button.Left = 10; // x 좌표는 고정
+                    MenuInfo menuInfo = new MenuInfo();
+                    menuInfo.menu_name = item_name;
+
+                    button.Click += sql_btn_click;
+                    
+                    mysql_tab.Controls.Add(button);
+
+                    Console.WriteLine($"Button added: Name={button.Name}, Text={button.Text}, Top={button.Top}, Left={button.Left}");
+
+                    buttonTop += buttonSpacing; // 다음 버튼의 y 좌표 설정
+                }
+            }
+            con.Close();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test!");
         }
 
         private void Order_Manage_Load(object sender, EventArgs e)
@@ -60,7 +123,11 @@ namespace Kiosk.Order
             timer.Start();
         }
 
-
+        private class MenuInfo
+        {
+            public string menu_name { get; set; }
+        }
+        
         private int menu_price(string menu_name)
         {
             int price = 0;
@@ -155,6 +222,11 @@ namespace Kiosk.Order
             payment.Text = payments.Sum()+"";
         }
 
+        private void sql_btn_click(object sender, EventArgs e)
+        {
+            MessageBox.Show("동적 생성 버튼","Welcome",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             select_menu(coffee_ice_menu1.Text);
@@ -205,6 +277,9 @@ namespace Kiosk.Order
             select_menu(coffee_hot_menu5.Text);
         }
 
-       
+        private void mysql_menu1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
