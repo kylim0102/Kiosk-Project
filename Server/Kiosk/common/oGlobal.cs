63a1425ac2db19e2d9c454f4e0b87f35401a1823
wkkjhhhhh <qw8421@naver.com>
2024-07-11 13:29:57 +0900

    format

--- Server/Kiosk/common/oGlobal.cs
+++ Server/Kiosk/common/oGlobal.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using MySql.Data.MySqlClient;
 
 namespace Kiosk.pPanel.common
@@ -47,17 +50,96 @@ namespace Kiosk.pPanel.common
         #endregion
     }
 
-
     internal class StorageConnection
     {
-        // Azure Storage 연결 문자열 초기화
-        private string connectionString = null;
-        // Azure Storage Name
-        private string contanerName = null;
-
-        public static BlobClient GetBlobClient()
+        // Azure Storage Connection
+        public BlobContainerClient BlobContainerClient()
         {
-            return null;
+            // Azure Sotrage 연결 문자열
+            string connectionString1 = "DefaultEndpointsProtocol=https;";
+            string connectionString2 = "AccountName=kioskproject;";
+            string connectionString3 = "AccountKey=7WdD4X1zax7SKV5ouf+svEtku2v7buHUWt/BeNNsY0mS4kETH/UyZP4WpaNbi7LpxPRoobEUezWY+AStK0dOGA==;";
+            string connectionString4 = "EndpointSuffix=core.windows.net";
+
+
+            // Azure Storage 이름
+            string containerName = "kiosk";
+            //Azure Storage 연결
+            BlobContainerClient containerClient = new BlobContainerClient(connectionString1 + connectionString2 + connectionString3 + connectionString4, containerName);
+
+            return containerClient;
+        }
+
+        // Azure Storage List Show
+        public List<BlobItem> GetBlobs()
+        {
+            // 컨테이너 클라이언트 생성
+            var containerClient = BlobContainerClient();
+
+            int size = containerClient.GetBlobs().Count();
+            List<BlobItem> items = containerClient.GetBlobs().ToList();
+
+            return items;
+        }
+
+        // Azure Storage File Upload
+        // 업로드 성공 시 true 반환, 실패 시 false 반환
+        public bool Upload(string filepath)
+        {
+            string blobName = Path.GetFileName(filepath);
+
+            // 컨테이너 클라이언트 생성
+            var containerClient = BlobContainerClient();
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+
+            try
+            {
+                // 파일 스트림을 열어 Blob에 업로드
+                FileStream fileStream = File.OpenRead(filepath);
+                blobClient.Upload(fileStream, true);
+                fileStream.Close();
+                MessageBox.Show("선택한 파일이 Storage로 업로드되었습니다.", "Success Upload !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("파일 업로드에 실패했습니다.", "Fail Upload", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public async Task Download(string blobName, string downloadFilepath)
+        {
+            /*
+                blobName : Storage에서 다운로드하고자 하는 파일 명 
+            */
+
+            // Storage 연결
+            var containerClient = BlobContainerClient();
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            bool exists = await blobClient.ExistsAsync();
+
+            if (!exists) // 다운로드 하려는 파일이 Storage에 없는 경우
+            {
+                MessageBox.Show("Storage에 해당 파일이 존재하지 않습니다.", "Download ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("다운로드를 시작합니다.", "Download", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Blob의 내용을 다운로드하여 BlobDownloadInfo 객체에 저장
+                BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+                // 로컬 Storage에 저장
+                using (FileStream fs = File.OpenWrite(downloadFilepath))
+                {
+                    await download.Content.CopyToAsync(fs);
+                    MessageBox.Show("다운로드 성공", "Download Success !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    fs.Close();
+                }
+            }
         }
     }
 
