3279da04422022df0f0726aebc76797ea87d3cf9
hoon <ldh970527@naver.com>
2024-07-13 14:39:13 +0900

    MySql Directory Update 1.1 And Category Penal Update 1.1

--- Server/Kiosk/ItemManage/ItemPanel/CategoryPanel.cs
+++ Server/Kiosk/ItemManage/ItemPanel/CategoryPanel.cs
@@ -15,6 +15,7 @@ namespace Kiosk.ItemManage.ItemPanel
     public partial class Category_Manage : UserControl
     {
         private MySqlConnection mysql = oGlobal.GetConnection();
+        private CategoryTable table = new CategoryTable();
         private string sql = null;
 
         public Category_Manage()
@@ -22,50 +23,17 @@ namespace Kiosk.ItemManage.ItemPanel
             InitializeComponent();
         }
 
-        // 데이터베이스가 비어있는지 여부 확인
-        private int null_check()
-        {
-            int result = 0;
-            sql = "select count(*) as count from categorytable";
-            MySqlCommand cmd = new MySqlCommand(sql, mysql);
-            
-            using(MySqlDataReader reader = cmd.ExecuteReader())
-            {
-                reader.Read();
-
-                result = reader.GetInt32("count");
-            }
-            return result;
-        }
-
         private void Category_Manage_Load(object sender, EventArgs e)
         {
-            try
+            // 데이터베이스가 비어 있다면 카테고리 코드를 10으로 등록
+            if (table.CategoryTableCount() == 0)
             {
-                // 데이터베이스가 비어 있다면 카테고리 코드를 10으로 등록
-                if (null_check() == 0)
-                {
-                    Category_code.Text = "10";
-                }
-                // 비어있지 않다면 현재 카테고리 코드의 최댓값에서 +10을 하여 등록
-                else 
-                {
-                    sql = "select max(cg_code) as max from categorytable";
-
-                    MySqlCommand cmd = new MySqlCommand(sql, mysql);
-
-                    using (MySqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        reader.Read();
-                        int max_code = reader.GetInt32("max") + 10;
-
-                        Category_code.Text = max_code.ToString();
-                    }
-                }
+                Category_code.Text = "10";
             }
-            catch(MySqlException ex)
+            // 비어있지 않다면 현재 카테고리 코드의 최댓값에서 +10을 하여 등록
+            else 
             {
-                MessageBox.Show(ex.Message, "MYSQL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Category_code.Text = table.CategoryMaxCode().ToString();
             }
         }
 
@@ -77,31 +45,8 @@ namespace Kiosk.ItemManage.ItemPanel
 
         private void Register_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string now = DateTime.Now.ToString("yyyy-MM-dd");
-                sql = "insert into categorytable(cg_code, cg_name, regdate) values(@cg_code, @cg_name, regdate)";
-                MySqlCommand cmd = new MySqlCommand(sql, mysql);
-
-                cmd.Parameters.AddWithValue("@cg_code",Category_code.Text);
-                cmd.Parameters.AddWithValue("@cg_name",Category_name.Text);
-                cmd.Parameters.AddWithValue("@regdate", now);
-
-                int result = cmd.ExecuteNonQuery();
-                if (result < 0)
-                {
-                    MessageBox.Show("카테고리 등록에 실패했습니다! \n관리자에게 문의하세요.", "CODE : MS-ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                else
-                {
-                    MessageBox.Show("카테고리 등록에 성공했습니다!", "CATEGORY REGISTER", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    Category_name.Text = string.Empty;
-                }
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show(ex.Message, "MYSQL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            int result = table.CategoryRegister(Category_code.Text, Category_name.Text);
+            Category_name.Text = string.Empty;
         }
     }
 }
--- Server/Kiosk/common/Mysql.cs
+++ Server/Kiosk/common/Mysql.cs
@@ -11,11 +11,105 @@ using System.Windows.Forms.DataVisualization.Charting;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
 
 namespace Kiosk.pPanel.common
 {
-    internal class Mysql
+    #region To CategoryPanel.cs
+    internal class CategoryTable
+    {
+        private MySqlConnection mysql = oGlobal.GetConnection();
+        private string sql = null;
+        int result = 0;
+
+        public int CategoryTableCount()
+        {
+            try
+            {
+                sql = "select count(*) as count from categorytable";
+                MySqlCommand cmd = new MySqlCommand(sql, mysql);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    result = reader.GetInt32("count");
+                }
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "MYSQL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result;
+        }
+        
+        public int CategoryRegister(string category_code, string category_name)
+        {
+            try
+            {
+                string now = DateTime.Now.ToString("yyyy-MM-dd");
+                sql = "insert into categorytable(cg_code, cg_name, regdate) values(@cg_code, @cg_name, regdate)";
+                MySqlCommand cmd = new MySqlCommand(sql, mysql);
+
+                cmd.Parameters.AddWithValue("@cg_code", category_code);
+                cmd.Parameters.AddWithValue("@cg_name", category_name);
+                cmd.Parameters.AddWithValue("@regdate", now);
+
+                result = cmd.ExecuteNonQuery();
+                if (result < 0)
+                {
+                    MessageBox.Show("카테고리 등록에 실패했습니다! \n관리자에게 문의하세요.", "CODE : MS-ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("카테고리 등록에 성공했습니다!", "CATEGORY REGISTER", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "MYSQL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result;
+        }
+
+        public int CategoryMaxCode()
+        {
+            try
+            {
+                sql = "select max(cg_code) as max from categorytable";
+
+                MySqlCommand cmd = new MySqlCommand(sql, mysql);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    result = reader.GetInt32("max") + 10;
+                }
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "MYSQL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result;
+        }
+    }
+    #endregion
+
+    #region To ItemPanel.cs
+    internal class ItemTable
     {
 
     }
+    #endregion
+
+
+    #region To OptionPanel.cs
+    internal class OptionTable
+    {
+
+    }
+    #endregion
 }
