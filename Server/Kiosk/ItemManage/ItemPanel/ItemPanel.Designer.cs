7deb8ef7317ea40fd77ed4e1a9f117854e7580d2
wkkjhhhhh <qw8421@naver.com>
2024-07-14 00:30:09 +0900

    파일 등록 패널 변경

--- Server/Kiosk/ItemManage/ItemPanel/ItemPanel.Designer.cs
+++ Server/Kiosk/ItemManage/ItemPanel/ItemPanel.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Item_Register = new System.Windows.Forms.GroupBox();
             this.Reset = new System.Windows.Forms.Button();
             this.Register = new System.Windows.Forms.Button();
             this.Item_category = new System.Windows.Forms.ComboBox();
@@ -43,37 +42,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.Item_Register.SuspendLayout();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // Item_Register
-            // 
-            this.Item_Register.Controls.Add(this.Reset);
-            this.Item_Register.Controls.Add(this.Register);
-            this.Item_Register.Controls.Add(this.Item_category);
-            this.Item_Register.Controls.Add(this.label5);
-            this.Item_Register.Controls.Add(this.file_path);
-            this.Item_Register.Controls.Add(this.file_scan);
-            this.Item_Register.Controls.Add(this.label4);
-            this.Item_Register.Controls.Add(this.Item_content);
-            this.Item_Register.Controls.Add(this.Item_price);
-            this.Item_Register.Controls.Add(this.Item_name);
-            this.Item_Register.Controls.Add(this.label3);
-            this.Item_Register.Controls.Add(this.label2);
-            this.Item_Register.Controls.Add(this.label1);
-            this.Item_Register.Location = new System.Drawing.Point(121, 3);
-            this.Item_Register.Name = "Item_Register";
-            this.Item_Register.Size = new System.Drawing.Size(490, 403);
-            this.Item_Register.TabIndex = 20;
-            this.Item_Register.TabStop = false;
-            this.Item_Register.Text = "Item_Register";
-            this.Item_Register.Enter += new System.EventHandler(this.Item_Register_Enter);
             // 
             // Reset
             // 
-            this.Reset.Location = new System.Drawing.Point(267, 357);
+            this.Reset.Location = new System.Drawing.Point(442, 429);
+            this.Reset.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Reset.Name = "Reset";
-            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.Size = new System.Drawing.Size(246, 29);
             this.Reset.TabIndex = 35;
             this.Reset.Text = "리셋";
             this.Reset.UseVisualStyleBackColor = true;
@@ -81,9 +62,10 @@
             // 
             // Register
             // 
-            this.Register.Location = new System.Drawing.Point(151, 357);
+            this.Register.Location = new System.Drawing.Point(69, 429);
+            this.Register.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Register.Name = "Register";
-            this.Register.Size = new System.Drawing.Size(75, 23);
+            this.Register.Size = new System.Drawing.Size(249, 29);
             this.Register.TabIndex = 34;
             this.Register.Text = "등록";
             this.Register.UseVisualStyleBackColor = true;
@@ -93,35 +75,36 @@
             // 
             this.Item_category.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.Item_category.FormattingEnabled = true;
-            this.Item_category.Location = new System.Drawing.Point(101, 39);
+            this.Item_category.Location = new System.Drawing.Point(203, 40);
+            this.Item_category.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Item_category.Name = "Item_category";
-            this.Item_category.Size = new System.Drawing.Size(367, 20);
+            this.Item_category.Size = new System.Drawing.Size(370, 23);
             this.Item_category.TabIndex = 31;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(29, 42);
+            this.label5.Location = new System.Drawing.Point(121, 43);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.Size = new System.Drawing.Size(67, 15);
             this.label5.TabIndex = 30;
             this.label5.Text = "카테고리";
             // 
             // file_path
             // 
-            this.file_path.Location = new System.Drawing.Point(101, 310);
+            this.file_path.Location = new System.Drawing.Point(203, 379);
             this.file_path.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.file_path.Name = "file_path";
             this.file_path.ReadOnly = true;
-            this.file_path.Size = new System.Drawing.Size(274, 21);
+            this.file_path.Size = new System.Drawing.Size(370, 25);
             this.file_path.TabIndex = 29;
             // 
             // file_scan
             // 
-            this.file_scan.Location = new System.Drawing.Point(383, 310);
+            this.file_scan.Location = new System.Drawing.Point(591, 379);
             this.file_scan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.file_scan.Name = "file_scan";
-            this.file_scan.Size = new System.Drawing.Size(85, 23);
+            this.file_scan.Size = new System.Drawing.Size(144, 29);
             this.file_scan.TabIndex = 27;
             this.file_scan.Text = "찾아보기";
             this.file_scan.UseVisualStyleBackColor = true;
@@ -130,62 +113,62 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(29, 313);
+            this.label4.Location = new System.Drawing.Point(121, 382);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.Size = new System.Drawing.Size(72, 15);
             this.label4.TabIndex = 12;
             this.label4.Text = "제품 사진";
             // 
             // Item_content
             // 
-            this.Item_content.Location = new System.Drawing.Point(101, 212);
+            this.Item_content.Location = new System.Drawing.Point(203, 256);
             this.Item_content.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Item_content.Multiline = true;
             this.Item_content.Name = "Item_content";
-            this.Item_content.Size = new System.Drawing.Size(367, 65);
+            this.Item_content.Size = new System.Drawing.Size(370, 94);
             this.Item_content.TabIndex = 11;
             // 
             // Item_price
             // 
-            this.Item_price.Location = new System.Drawing.Point(101, 149);
+            this.Item_price.Location = new System.Drawing.Point(203, 177);
             this.Item_price.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Item_price.Name = "Item_price";
-            this.Item_price.Size = new System.Drawing.Size(367, 21);
+            this.Item_price.Size = new System.Drawing.Size(370, 25);
             this.Item_price.TabIndex = 10;
             // 
             // Item_name
             // 
-            this.Item_name.Location = new System.Drawing.Point(101, 88);
+            this.Item_name.Location = new System.Drawing.Point(203, 101);
             this.Item_name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Item_name.Name = "Item_name";
-            this.Item_name.Size = new System.Drawing.Size(367, 21);
+            this.Item_name.Size = new System.Drawing.Size(370, 25);
             this.Item_name.TabIndex = 9;
             this.Item_name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 212);
+            this.label3.Location = new System.Drawing.Point(121, 256);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.Size = new System.Drawing.Size(67, 15);
             this.label3.TabIndex = 8;
             this.label3.Text = "제품설명";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 152);
+            this.label2.Location = new System.Drawing.Point(121, 181);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.Size = new System.Drawing.Size(67, 15);
             this.label2.TabIndex = 7;
             this.label2.Text = "제품가격";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 91);
+            this.label1.Location = new System.Drawing.Point(121, 105);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 6;
             this.label1.Text = "제품명";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -194,23 +177,66 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(813, 508);
+            this.tabControl1.TabIndex = 21;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Reset);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.Register);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.Item_category);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.file_path);
+            this.tabPage1.Controls.Add(this.Item_name);
+            this.tabPage1.Controls.Add(this.file_scan);
+            this.tabPage1.Controls.Add(this.Item_price);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.Item_content);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(805, 479);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "제품 등록";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(805, 479);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "제품 수정";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // ItemPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.Item_Register);
+            this.Controls.Add(this.tabControl1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ItemPanel";
-            this.Size = new System.Drawing.Size(714, 409);
+            this.Size = new System.Drawing.Size(816, 511);
             this.Load += new System.EventHandler(this.ItemPanel_Load);
-            this.Item_Register.ResumeLayout(false);
-            this.Item_Register.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.GroupBox Item_Register;
         private System.Windows.Forms.TextBox Item_content;
         private System.Windows.Forms.TextBox Item_price;
         private System.Windows.Forms.TextBox Item_name;
@@ -225,5 +251,8 @@
         private System.Windows.Forms.Button Reset;
         private System.Windows.Forms.Button Register;
         public System.Windows.Forms.ComboBox Item_category;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
